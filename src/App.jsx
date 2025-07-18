b9c15818f33a62d385ab4024a445a5069bfc2218
Batubo Victory <victorybatubo76@gmail.com>
2025-07-18 18:02:50 +0100

    Prep for usestate

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
-
+import Popup from "./components/Popup";
+import Todo from "./components/Todo";
+import { useState } from "react";
 
 function App() {
-
   return (
     <>
+      <Todo task="Learn React" />
+      <Todo task="Build a project" />
+      <Todo task="Apply for jobs" />
+      <Todo task="Chill and flex" />
       
     </>
   )
