aae7e5a27caee79971c3127074ace77d90868e2e
Batubo Victory <victorybatubo76@gmail.com>
2025-07-18 17:17:16 +0100

    Add input and label

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -2,6 +2,16 @@ import Popup from "./Popup";
 
 function Todo(){
 
+  let task;
+  return(
+    <>
+    <label htmlFor="task">Input Your Task:</label>
+    <input type="text" onChange={(e) => {
+      task = e.target.value;
+    }} />
+    <Popup task= {task} />
+  </>
+  )
 }
 
 export default Todo
