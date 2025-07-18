b375ec8ef317b963bb8e88f4b7e4f2d4b0988009
Batubo Victory <victorybatubo76@gmail.com>
2025-07-18 17:00:52 +0100

    Reset Popup component

--- src/components/Popup.jsx
+++ src/components/Popup.jsx
@@ -1,33 +1,9 @@
-function Popup({ percent, name, age }) {
-  const task = `Are you ${percent}% sure`;
-
-  function confirmation(){
-      console.log(name)
-    }
-  function log(e){
-    console.log(e.target.value);
-  }
+function Popup() {
+ 
   return (
 
     <>
-      <div className="popup">
-        <h1 className="PopQuestion">
-          {task} {name}?
-        </h1>
-        <p>You are just {age} years old</p>
-        <div className="flex">
-          <button
-            className="yes"
-            onClick={confirmation}
-          >
-            Yes
-          </button>
-          <button className="no">No</button>
-        </div><br />
-
-        <label htmlFor="">Name</label>
-        <input type="text" placeholder="Your Name" onChange={log} />
-      </div>
+      
     </>
   );
 }
