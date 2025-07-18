36cfc930330fa372ac8cc47b9b2d6cc8716aca5b
Batubo Victory <victorybatubo76@gmail.com>
2025-07-18 18:09:36 +0100

    Try UseState

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,16 @@ import Todo from "./components/Todo";
 import { useState } from "react";
 
 function App() {
+  const [popUpOpen, setPopUpOpen] = useState(false);
+  
+  
   return (
     <>
       <Todo task="Learn React" />
       <Todo task="Build a project" />
       <Todo task="Apply for jobs" />
       <Todo task="Chill and flex" />
-      
+      {popUpOpen && <Popup /> /*if statement*/} 
     </>
   )
 }
