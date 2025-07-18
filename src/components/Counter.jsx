2eab6cf09bade280f7723ceec11269f5b1aa7e06
Batubo Victory <victorybatubo76@gmail.com>
2025-07-18 18:19:57 +0100

    Export counter component

---
+++ src/components/Counter.jsx
@@ -0,0 +1,12 @@
+import { useState} from 'react';
+
+function Counter(){
+  return(
+    <>
+      <h1>Counter: {}</h1>
+      <button>Increment</button><button>Decrement</button><button>Reset</button>
+    </>
+  )
+}
+
+export default Counter
