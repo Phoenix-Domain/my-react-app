98fcbc05a3007de73e32c0ea88369c8d3f54741f
Batubo Victory <victorybatubo76@gmail.com>
2025-07-18 17:41:57 +0100

    Start over

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -2,15 +2,10 @@ import Popup from "./Popup";
 
 function Todo(){
 
-  let task;
-  return(
+  return (
     <>
-    <label htmlFor="task">Input Your Task:</label>
-    <input type="text" onChange={(e) => {
-      task = e.target.value;
-    }} />
-    <Popup task= {task} />
-  </>
+    
+    </>
   )
 }
 
