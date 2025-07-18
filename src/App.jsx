96b5e1444a116ebee9c22a4c350e1ebd8c85f117
Batubo Victory <victorybatubo76@gmail.com>
2025-07-18 16:56:33 +0100

    Remove Popup component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-import Popup from "./components/Popup"
+
 
 function App() {
 
   return (
     <>
-      <Popup name="Victory" age={20} percent="90"/>
-      <Popup percent='79' name="Batubo" age={50}/>
+      
     </>
   )
 }
