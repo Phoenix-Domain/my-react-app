c9e2a94707a22351b90e40406c64bc91a0c9a1b8
Batubo Victory <victorybatubo76@gmail.com>
2025-07-18 10:34:14 +0100

    Remove Popup components

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,5 @@ import Popup from './components/Popup.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-    <Popup name="Victory" age={20} percent="90"/>
-    <Popup percent='79' name="Batubo" age={50}/>
   </StrictMode>,
 )
