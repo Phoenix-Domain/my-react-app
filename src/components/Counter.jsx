b17ecca3066ca32bf2d51f589123826969560ea9
Batubo Victory <victorybatubo76@gmail.com>
2025-07-19 13:31:29 +0100

    First tryyyyyyy

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,7 +1,20 @@
 import { useState } from 'react';
 
 function Counter(){
+  const [count, setCount] = useState(0);
 
+  return(
+    <>
+      <h1>Count: {count}</h1>
+      <div>
+        <button onClick={() => {setCount(prevCount => prevCount + 1)}}>Increment</button>
+
+        <button onClick={() => {setCount(prevCount => prevCount - 1)}}>Decrement</button>
+        <button onClick={() => {setCount(0)}}>Reset</button>
+      </div>
+    </>
+  )
 }
+
 
 export default Counter;
