a91e7733e38fb50e42812ecdf4b7ec70b759568e
Batubo Victory <victorybatubo76@gmail.com>
2025-07-29 15:03:13 +0100

    Add increment onclick

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -2,13 +2,15 @@ import { useState } from "react";
 
 function Counter(){
 
-  useState(0)
+  const [count, setCount] = useState(0)
 
   return(
     <>
       <h1 className="header">Counter App</h1>
-      <h3>Count: 0</h3>
-      <button>Increment</button>
+      <h3>Count: {count}</h3>
+      <button onClick={() => {
+        setCount(prevCount => prevCount + 1)
+      }}>Increment</button>
       <button>Decreament</button>
       <button>Reset</button>
     </>
