c0c32318072bf1f9bf10aaf9bd5606c50af1bebb
Batubo Victory <victorybatubo76@gmail.com>
2025-07-29 14:47:57 +0100

    Create counter components

---
+++ src/components/Counter.jsx
@@ -0,0 +1,15 @@
+
+function Counter(){
+
+  return(
+    <>
+      <h1>Counter App</h1>
+      <h3>Count: 0</h3>
+      <button>Increment</button>
+      <button>Decreament</button>
+      <button>Reset</button>
+    </>
+  )
+}
+
+export default Counter;
