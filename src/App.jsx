480a58e6f55731b236dd6b608c58ed91303a597e
Batubo Victory <victorybatubo76@gmail.com>
2025-07-31 18:50:58 +0100

    Create posts page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from './pages/home'
 import Contact from './pages/contact'
 import About from './pages/about'
+import Posts from './pages/Posts'
 
 
 function App() {
@@ -9,9 +10,10 @@ function App() {
   return (
    <Router>
       <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/about" element={<About />}></Route>
-        <Route path="/contact" element={<Contact />}></Route>
+        <Route path="/" element={<Home />}/>
+        <Route path="/about" element={<About />}/>
+        <Route path="/contact" element={<Contact />}/>
+        <Route path="/Posts" element={<Posts />} />
 
 
       </Routes>
