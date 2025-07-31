6a206f00a6f985e24b25d74f7cd071e0bc7f706c
Batubo Victory <victorybatubo76@gmail.com>
2025-07-31 16:11:42 +0100

    Add name and age headers

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -2,13 +2,14 @@ import { useState } from "react";
 
 function Counter(){
 
-  const [user, setUser] = useState({name: 'victory', age: 12})
+  const [user, setUser] = useState({name: 'victory', age: 21})
 
   return(
     <>
-      <h1 className="header">Counter App</h1>
+      <h1 className="header">Info App</h1>
 
-      <h3></h3>
+      <h3>Name: {user.name.toUpperCase()}</h3>
+      <h3>Age: {user.age}</h3>
 
       <button>Increment</button>
 
