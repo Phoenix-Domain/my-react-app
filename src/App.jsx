9fd6c570f122f0dac14f29cc7b9ec5037413a037
Batubo Victory <victorybatubo76@gmail.com>
2025-08-01 10:30:55 +0100

    Start over again

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
 import Home from './pages/home'
 import Contact from './pages/contact'
 import About from './pages/about'
