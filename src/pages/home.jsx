1670ff07d611e18efcfcc60eafd5abf15cc689d7
Batubo Victory <victorybatubo76@gmail.com>
2025-08-01 13:15:19 +0100

    Add an async function

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,6 +1,24 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios'
+
 function Home(){
+
+  const [users, setUsers] = useState([]);
+
+  async function fetchData(){
+    const {data} = await axios.get('https://jsonplaceholder.typicode.com/users');
+    
+    setUsers(data);
+  };
+
+  useEffect(() => {
+    fetchData();
+  },[users])
   return(
-    <h1>Home</h1>
+    <>
+      <h1>Home</h1>
+      <p></p>
+    </>
   )
 }
 
