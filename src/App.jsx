f71aa87a6cf235851fc6991f04d65092152d25c6
Batubo Victory <victorybatubo76@gmail.com>
2025-08-01 10:39:26 +0100

    Redo everything

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,30 @@
-
-import Home from './pages/home'
-import Contact from './pages/contact'
-import About from './pages/about'
-import Posts from './pages/Posts'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Home from './pages/home';
+import Contact from './pages/contact';
+import About from './pages/about';
+import Posts from './pages/Posts';
 
 
 function App() {
 
   return (
-   
+   <Router>
+    <nav>
+      <Link to='/'>Home</Link>
+      <Link to='/Contact'>Contact</Link>
+      <Link to='/About'>About</Link>
+      <Link to='/Posts'>Posts</Link>
+    </nav>
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path="/Contact" element={<Contact />} />
+      <Route path="/About" element={<About />} />
+      <Route path="/Posts" element={<Posts />} />
+    </Routes>
+    <>
+    
+    </>
+   </Router>
   )
 }
 
