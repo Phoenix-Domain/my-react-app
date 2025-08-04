afb7262c01f2b10c1a1e83a2c51ebb78a9a7f80b
Batubo Victory <victorybatubo76@gmail.com>
2025-08-04 12:53:33 +0100

    Display user info dynamically

--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -1,12 +1,27 @@
 import {useParams} from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import axios from 'axios'
 
 
 function Users(){
-  const { username } = useParams();
+  const { id } = useParams();
+  const [data, setData] = useState([])
+
+  async function getData(){
+    const {data} = await axios.get(`https://jsonplaceholder.typicode.com/users/${id}`);
+
+    setData(data);
+  }
+
+  useEffect(() => {
+    getData();
+  },[])
 
   return(
     <div>
-      <h1>{username}</h1>
+      <h1>{data.name}</h1>
+      <h2>{data.username}</h2>
+      <h3>{data.id}</h3>
     </div>
   )
 }
