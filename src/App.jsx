2d8fdf20726dbf189329a5fe1599ea43ddcc8081
Batubo Victory <victorybatubo76@gmail.com>
2025-08-05 14:35:05 +0100

    Clear everything and restart

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,9 @@
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import Nav from './components/Nav';
-import Home from './pages/home';
-import Contact from './pages/contact';
-import About from './pages/about';
-import Posts from './pages/Posts';
-import Users from './pages/Users'
-
 function App() {
 
   return (
-   <Router>
-    <Nav />
-    <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/Users/:id' element={<Users /> } />
-    </Routes>
-    <>
-    
-    </>
-   </Router>
+   <>
+   
+   </>
   )
 }
 
