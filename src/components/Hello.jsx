ca1f7eaba88767a7792d8bf90c53d33dd01bff47
Batubo Victory <victorybatubo76@gmail.com>
2025-08-05 15:14:44 +0100

    Change content

--- src/components/Hello.jsx
+++ src/components/Hello.jsx
@@ -1,8 +1,7 @@
 function Hello(){
   return(
     <>
-      <h1 className="text-5xl my-4">Batubo Victory</h1>
-      <p>LFC Ozuoba</p>
+      <h1 className="text-5xl text-center my-4">My Name is Batubo Victory</h1>
     </>
   )
 }
