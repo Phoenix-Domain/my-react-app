efa615e364cbf79696cd2192f1b7cad2dc969552
Batubo Victory <victorybatubo76@gmail.com>
2025-08-05 14:58:37 +0100

    Import Header and Footer components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
-import Hello from './components/Hello'
+import Hello from './components/Hello';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
 function App() {
 
   return (
    <>
     <Hello />
+    <Header />
+    <Footer />
    </>
   )
 }
