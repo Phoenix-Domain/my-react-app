6506c7737234a468ad9f5d54d4c7593d7cff28c4
Batubo Victory <victorybatubo76@gmail.com>
2025-08-05 15:11:47 +0100

    Import Hello and Welcome components

---
+++ src/components/Body.jsx
@@ -0,0 +1,13 @@
+import Hello from './Hello'
+import Welcome from './Welcome'
+
+function Body(){
+  return(
+    <>
+      <Hello />
+      <Welcome name="Phoenix Domain" message="Welcome to my Domain" />
+    </>
+  )
+}
+
+export default Body;
