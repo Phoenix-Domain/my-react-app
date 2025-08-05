58b1076efbe075768e94863c4baee09b71667fae
Batubo Victory <victorybatubo76@gmail.com>
2025-08-05 15:15:41 +0100

    Change content

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -2,7 +2,7 @@ function Welcome({ name, message }){
   return(
     <>
       <h1 className="text-4xl my-4">
-        {name}
+        This is {name}
       </h1>
 
       <h2 className="text-2xl my-4">
