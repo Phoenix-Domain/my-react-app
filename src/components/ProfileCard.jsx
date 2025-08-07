3d1c7d56397b06b70b1a7dd99478e18808838b64
Batubo Victory <victorybatubo76@gmail.com>
2025-08-07 12:30:44 +0100

    Add useState variables for name, age ad location

--- src/components/ProfileCard.jsx
+++ src/components/ProfileCard.jsx
@@ -1,6 +1,12 @@
 import { useState } from 'react';
 
 function ProfileCard(){
+  const [name, setName] = useState('Victory');
+
+  const [age, setAge] = useState(21);
+
+  const [location, setLocation] = useState('LFC Ozuoba');
+
   return(
     <>
       <p>Name: </p>
