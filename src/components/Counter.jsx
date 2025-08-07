fda9fd0e964a42df330d8f39ab602e09d1a9cd10
Batubo Victory <victorybatubo76@gmail.com>
2025-08-07 12:19:58 +0100

    Add styling to button

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -5,7 +5,7 @@ function Counter (){
 
   return(
     <>
-      <button>Count: {count}</button>
+      <button className='border-1 py-2 px-5 rounded-full bg-blue-900 text-white font-bold text-2xl'>Count: {count}</button>
     </>
   )
 }
