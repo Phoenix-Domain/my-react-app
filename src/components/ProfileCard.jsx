0a992f882567b28f0efc468ca1c17bc0ede3b4b1
Batubo Victory <victorybatubo76@gmail.com>
2025-08-07 12:39:36 +0100

    Add onClick event handlers to update state variables

--- src/components/ProfileCard.jsx
+++ src/components/ProfileCard.jsx
@@ -12,19 +12,19 @@ function ProfileCard(){
       <p className="mt-5 ml-3 font-bold">
         Name: {name}
 
-        <button className='border-1 py-2 px-5 rounded-full block mb-5 bg-purple-900 text-white font-bold text-xl active:bg-blue-900'>Change Name</button>
+        <button className='border-1 py-2 px-5 rounded-full block mb-5 bg-purple-900 text-white font-bold text-xl active:bg-blue-900' onClick={() => {setName('Batubo')}}>Change Name</button>
       </p>
 
       <p className="mt-5 ml-3 font-bold">
         Age: {age}
 
-        <button className='border-1 py-2 px-5 rounded-full block mb-5 bg-purple-900 text-white font-bold text-xl active:bg-blue-900'>Change Age</button>
+        <button className='border-1 py-2 px-5 rounded-full block mb-5 bg-purple-900 text-white font-bold text-xl active:bg-blue-900' onClick={() => {setAge(42)}}>Change Age</button>
       </p>
 
       <p className="mt-5 ml-3 font-bold">
         Location: {location}
 
-        <button className='border-1 py-2 px-5 rounded-full block mb-5 bg-purple-900 text-white font-bold text-xl active:bg-blue-900'>Change Location</button>
+        <button className='border-1 py-2 px-5 rounded-full block mb-5 bg-purple-900 text-white font-bold text-xl active:bg-blue-900' onClick={() => {setLocation('Covenant University')}}>Change Location</button>
       </p>
 
       
