662a948cf15452302b766cb6caba1ca96a35893b
Batubo Victory <victorybatubo76@gmail.com>
2025-08-07 12:36:24 +0100

    Add optional styling for clarity

--- src/components/ProfileCard.jsx
+++ src/components/ProfileCard.jsx
@@ -9,22 +9,22 @@ function ProfileCard(){
 
   return(
     <>
-      <p>
+      <p className="mt-5 ml-3 font-bold">
         Name: {name}
 
-        <button className='border-1 py-2 px-5 rounded-full bg-purple-900 text-white font-bold text-xl active:bg-blue-900'>Change Name</button>
+        <button className='border-1 py-2 px-5 rounded-full block mb-5 bg-purple-900 text-white font-bold text-xl active:bg-blue-900'>Change Name</button>
       </p>
 
-      <p>
+      <p className="mt-5 ml-3 font-bold">
         Age: {age}
 
-        <button className='border-1 py-2 px-5 rounded-full bg-purple-900 text-white font-bold text-xl active:bg-blue-900'>Change Age</button>
+        <button className='border-1 py-2 px-5 rounded-full block mb-5 bg-purple-900 text-white font-bold text-xl active:bg-blue-900'>Change Age</button>
       </p>
 
-      <p>
+      <p className="mt-5 ml-3 font-bold">
         Location: {location}
 
-        <button className='border-1 py-2 px-5 rounded-full bg-purple-900 text-white font-bold text-xl active:bg-blue-900'>Change Location</button>
+        <button className='border-1 py-2 px-5 rounded-full block mb-5 bg-purple-900 text-white font-bold text-xl active:bg-blue-900'>Change Location</button>
       </p>
 
       
