15a81ca280232ff0f38441b1aecab814b9aa5c58
Batubo Victory <victorybatubo76@gmail.com>
2025-08-07 12:24:26 +0100

    Add decrease count button

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -8,6 +8,10 @@ function Counter (){
       <button className='border-1 py-2 px-5 rounded-full bg-blue-900 text-white font-bold text-2xl active:bg-red-900' onClick={() => {setCount(prevCount => prevCount + 1)}}>
         Count: {count}
       </button>
+
+      <button className='border-1 py-2 px-5 rounded-full bg-blue-900 text-white font-bold text-2xl active:bg-red-900' onClick={() => {setCount(prevCount => prevCount - 1)}}>
+        Decrease Count
+      </button>
     </>
   )
 }
