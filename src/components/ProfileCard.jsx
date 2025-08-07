c2e3da28f09b66e8e0bcd769323147652deeec47
Batubo Victory <victorybatubo76@gmail.com>
2025-08-07 12:41:14 +0100

    Reset component

--- src/components/ProfileCard.jsx
+++ src/components/ProfileCard.jsx
@@ -1,35 +1,11 @@
 import { useState } from 'react';
 
 function ProfileCard(){
-  const [name, setName] = useState('Victory');
-
-  const [age, setAge] = useState(21);
-
-  const [location, setLocation] = useState('LFC Ozuoba');
-
-  return(
-    <>
-      <p className="mt-5 ml-3 font-bold">
-        Name: {name}
-
-        <button className='border-1 py-2 px-5 rounded-full block mb-5 bg-purple-900 text-white font-bold text-xl active:bg-blue-900' onClick={() => {setName('Batubo')}}>Change Name</button>
-      </p>
-
-      <p className="mt-5 ml-3 font-bold">
-        Age: {age}
-
-        <button className='border-1 py-2 px-5 rounded-full block mb-5 bg-purple-900 text-white font-bold text-xl active:bg-blue-900' onClick={() => {setAge(42)}}>Change Age</button>
-      </p>
-
-      <p className="mt-5 ml-3 font-bold">
-        Location: {location}
-
-        <button className='border-1 py-2 px-5 rounded-full block mb-5 bg-purple-900 text-white font-bold text-xl active:bg-blue-900' onClick={() => {setLocation('Covenant University')}}>Change Location</button>
-      </p>
-
-      
-    </>
-  )
+ return(
+  <>
+  
+  </>
+ )
 }
 
 export default ProfileCard;
