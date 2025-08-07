dc9cbf47f0e44ee94ab09c9feb4b193e2b15ba53
Batubo Victory <victorybatubo76@gmail.com>
2025-08-07 12:21:45 +0100

    Add onClick event handler

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -5,7 +5,9 @@ function Counter (){
 
   return(
     <>
-      <button className='border-1 py-2 px-5 rounded-full bg-blue-900 text-white font-bold text-2xl'>Count: {count}</button>
+      <button className='border-1 py-2 px-5 rounded-full bg-blue-900 text-white font-bold text-2xl' onClick={() => {setCount(prevCount => prevCount + 1)}}>
+        Count: {count}
+      </button>
     </>
   )
 }
