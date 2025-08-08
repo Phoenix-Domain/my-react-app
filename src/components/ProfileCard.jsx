17ad2b1130e306a25158e2b9e35a24283823ed59
Batubo Victory <victorybatubo76@gmail.com>
2025-08-08 15:56:10 +0100

    Remove previous sttes

--- src/components/ProfileCard.jsx
+++ src/components/ProfileCard.jsx
@@ -1,19 +1,19 @@
 import { useState } from 'react';
 
 function ProfileCard(){
-  const [input, setInput] = useState(null)
+  
 
-  const textValue = document.querySelector('#text')
+ 
 
  return(
   <>
     <form action="">
       <label htmlFor="name" className='block'>Name:
-      <input type="text" id="name" className="border-2 my-5 mx-3 py-1 px-1"  onChange={() => {setInput(textValue.value)}}/>
+      <input type="text" id="name" className="border-2 my-5 mx-3 py-1 px-1"/>
       </label>
 
       <label htmlFor="message" className='block'>Message:
-        <input type="text" id="message" className="border-2 my-5 mx-3 py-1 px-1"  onChange={() => {setInput(textValue.value)}}/>
+        <input type="text" id="message" className="border-2 my-5 mx-3 py-1 px-1"/>
       </label>
 
       <button className='border-1 py-2 px-5 rounded-full bg-blue-900 text-white font-bold text-2xl active:bg-red-900'>
@@ -21,7 +21,7 @@ function ProfileCard(){
       </button>
     </form>
 
-    <p>{input}</p>
+    <p></p>
   </>
  )
 }
