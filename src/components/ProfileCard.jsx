2d6eb53ec965f1a1aa8338cb39971fb07e271cef
Batubo Victory <victorybatubo76@gmail.com>
2025-08-08 15:53:59 +0100

    Add submit button

--- src/components/ProfileCard.jsx
+++ src/components/ProfileCard.jsx
@@ -15,8 +15,13 @@ function ProfileCard(){
       <label htmlFor="message" className='block'>Message:
         <input type="text" id="message" className="border-2 my-5 mx-3 py-1 px-1"  onChange={() => {setInput(textValue.value)}}/>
       </label>
-      <p>{input}</p>
+
+      <button className='border-1 py-2 px-5 rounded-full bg-blue-900 text-white font-bold text-2xl active:bg-red-900'>
+        Submit Message
+      </button>
     </form>
+
+    <p>{input}</p>
   </>
  )
 }
