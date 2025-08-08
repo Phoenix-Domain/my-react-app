aae92d47255829a3c6f550f4884304f4cc592f9c
Batubo Victory <victorybatubo76@gmail.com>
2025-08-08 15:48:15 +0100

    Restart Project

--- src/components/ProfileCard.jsx
+++ src/components/ProfileCard.jsx
@@ -1,15 +1,12 @@
 import { useState } from 'react';
 
 function ProfileCard(){
-  const [input, setInput] = useState(null)
-
-  const textValue = document.querySelector('#text')
+  
 
  return(
   <>
     <form action="">
-      <input type="text" id="text" className="border-2 my-5 mx-3 py-1 px-1"  onChange={() => {setInput(textValue.value)}}/>
-      <p>{input}</p>
+     
     </form>
   </>
  )
