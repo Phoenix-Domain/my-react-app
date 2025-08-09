bc4e64c67070625bdc0f8585eb9b8f923f274617
Batubo Victory <victorybatubo76@gmail.com>
2025-08-09 15:41:54 +0100

    Add new styles

--- src/components/ProfileCard.jsx
+++ src/components/ProfileCard.jsx
@@ -3,7 +3,7 @@ import { useState } from 'react';
 function ProfileCard(){
   return (
     <>
-      <form action="" className='border-1 p-2'>
+      <form action="" className='rounded-xl mt-3 w-fit mx-auto p-2 bg-purple-900 text-white py-2 pr-10'>
         <label className='block my-3 font-bold'>Name: 
           <input type="text" className='border-1 p-1 ml-2'/>
         </label>
@@ -12,7 +12,7 @@ function ProfileCard(){
           <input type="text" className='border-1 p-1 ml-2'/>
         </label>
 
-        <button className='bg-blue-800 py-2 px-4 rounded-full text-white font-bold' type="submit">
+        <button className='bg-blue-800 py-2 px-4 rounded-full text-white font-bold hover:bg-white hover:text-blue-800 ease-in duration-200' type="submit">
           submit
         </button>
       </form>
