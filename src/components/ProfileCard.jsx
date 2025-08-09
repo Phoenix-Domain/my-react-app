8742c9bb8dd9b37b093bda318df2c6e6e13495a0
Batubo Victory <victorybatubo76@gmail.com>
2025-08-09 15:38:36 +0100

    Style form

--- src/components/ProfileCard.jsx
+++ src/components/ProfileCard.jsx
@@ -3,7 +3,19 @@ import { useState } from 'react';
 function ProfileCard(){
   return (
     <>
-    
+      <form action="" className='border-1 p-2'>
+        <label className='block my-3 font-bold'>Name: 
+          <input type="text" className='border-1 p-1 ml-2'/>
+        </label>
+        
+        <label className='block my-3 font-bold'>Message: 
+          <input type="text" className='border-1 p-1 ml-2'/>
+        </label>
+
+        <button className='bg-blue-800 py-2 px-4 rounded-full text-white font-bold' type="submit">
+          submit
+        </button>
+      </form>
     </>
   )
 }
