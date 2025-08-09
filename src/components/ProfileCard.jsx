392cdd804380c458a9baa4df02ab6c29947a4cf6
Batubo Victory <victorybatubo76@gmail.com>
2025-08-09 14:56:13 +0100

    Add state variable for name

--- src/components/ProfileCard.jsx
+++ src/components/ProfileCard.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 
 function ProfileCard(){
-  
+  const [name, setName] = useState('');
 
  return(
   <>
@@ -19,7 +19,7 @@ function ProfileCard(){
       </button>
     </form>
 
-    <p>My name is </p>
+    <p>My name is: {name}</p>
     <p>This is my message: </p>
   </>
  )
