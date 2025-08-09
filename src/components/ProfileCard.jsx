64c922e119e53e6bb5a9f44f5727c12946575057
Batubo Victory <victorybatubo76@gmail.com>
2025-08-09 15:22:11 +0100

    Handle state input correctly

--- src/components/ProfileCard.jsx
+++ src/components/ProfileCard.jsx
@@ -10,27 +10,31 @@ function ProfileCard(){
   function handleSubmit(e){
     e.preventDefault();
 
-    console.log(name)
+    const finalOutput = { name, message };
+
+    setOutput(finalOutput);
+
+    console.log(output)
   }
 
  return(
   <>
-    <form action="">
+    <form action="" onSubmit={e => handleSubmit(e)}>
       <label htmlFor="name" className='block'>Name:
-      <input type="text" id="name" className="border-2 my-5 mx-3 py-1 px-1"/>
+      <input type="text" id="name" className="border-2 my-5 mx-3 py-1 px-1" onChange={e => {setName(e.target.value)}}/>
       </label>
 
       <label htmlFor="message" className='block'>Message:
-        <input type="text" id="message" className="border-2 my-5 mx-3 py-1 px-1"/>
+        <input type="text" id="message" className="border-2 my-5 mx-3 py-1 px-1" onChange={e => {setMessage(e.target.value)}}/>
       </label>
 
-      <button className='border-1 py-2 px-5 rounded-full bg-blue-900 text-white font-bold text-2xl active:bg-red-900' onClick={e => handleSubmit(e)}>
+      <button className='border-1 py-2 px-5 rounded-full bg-blue-900 text-white font-bold text-2xl active:bg-red-900' type="submit">
         Submit Message
       </button>
     </form>
 
-    <p>My name is: {name}</p>
-    <p>This is my message: {message}</p>
+    <p>My name is: {output.name}</p>
+    <p>This is my message: {output.message}</p>
   </>
  )
 }
