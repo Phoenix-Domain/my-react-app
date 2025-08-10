b7b2800f6c501f60530afee6fec527b2bb043834
Batubo Victory <victorybatubo76@gmail.com>
2025-08-10 13:20:32 +0100

    Pass useState variables as props

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,14 +1,18 @@
 import { useState } from 'react';
 import Display from './Display'
+import Controls from './Controls'
 
 function Counter(){
-  const [count, setCount] = useState(0);
+  const [counter, setCounter] = useState(0);
 
   return (
     <>
       <h1 className='font-bold text-2xl text-center my-2'>
-        Count: <Display count={count} />
+        Counter App
       </h1>
+
+      <Display value={counter} />
+      <Controls setCounter={setCounter} />
     </>
   )
 }
