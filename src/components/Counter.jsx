112685f41867e5e97125a5e8899d8790608d224d
Batubo Victory <victorybatubo76@gmail.com>
2025-08-10 13:06:10 +0100

    Import display component

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,11 +1,14 @@
 import { useState } from 'react';
+import Display from './Display'
 
 function Counter(){
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <h1 cl>Count: {count}</h1>
+      <h1 className='font-bold text-2xl text-center my-2'>
+        Count: <Display count={count} />
+      </h1>
     </>
   )
 }
