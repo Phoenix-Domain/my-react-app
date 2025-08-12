60e8a497df1bc5eb24d5a353b6d2e1298cb14efa
Batubo Victory <victorybatubo76@gmail.com>
2025-08-12 10:44:29 +0100

    Style control content

--- src/components/Controls.jsx
+++ src/components/Controls.jsx
@@ -1,8 +1,13 @@
-function Controls(){
+function Controls({ setCounter }){
   return (
-    <>
-    
-    </>
+    <section>
+      <button className='text-3xl font-semibold bg-purple-500 block my-4 text-white py-2 px-4 rounded-xl '>
+        Increment Count
+      </button>
+      <button className='text-3xl font-semibold bg-purple-500 block my-4 text-white py-2 px-4 rounded-xl '>
+        Decrement Count
+      </button>
+    </section>
   )
 }
 
