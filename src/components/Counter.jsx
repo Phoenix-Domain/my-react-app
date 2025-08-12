9c00f931cf82191a3a1268aedaf44ea8698bcee9
Batubo Victory <victorybatubo76@gmail.com>
2025-08-12 10:35:00 +0100

    add content

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -3,9 +3,15 @@ import Display from './Display';
 import Controls from './Controls';
 
 function Counter(){
+  const [counter, setCounter] = useState(0);
+
   return(
     <>
+      <h1 className='text-center text-5xl font-bold text-purple-800'>Counter App</h1>
 
+      <Display counter={counter}/>
+
+      <Controls setCounter={setCounter()} />
     </>
   )
 }
