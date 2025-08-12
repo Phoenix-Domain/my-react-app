cb0645b8f082bc8702668a38bc4df1c723c37350
Batubo Victory <victorybatubo76@gmail.com>
2025-08-12 10:41:30 +0100

    Add styles

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,7 +1,7 @@
 function Display({ counter }){
   return(
     <>
-      <h3>Count: {counter}</h3>
+      <h3 className='text-3xl font-semibold bg-purple-500 inline-block py-2 px-4 rounded-xl text-gray-950 '>Count: {counter}</h3>
     </>
   )
 }
