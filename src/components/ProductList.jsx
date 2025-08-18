a95f0721541eda120a6f287f3111696443bc0f1b
Batubo Victory <victorybatubo76@gmail.com>
2025-08-18 14:47:29 +0100

    Use ternary operator to check premium products

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -5,17 +5,17 @@ function ProductList(){
   const [productArr, setProductArr] = useState([
     {
       name: "Product 1",
-      price: "$10.00",
+      price: 10,
       imageUrl: "https://via.placeholder.com/150"
     },
     {
       name: "Product 2",
-      price: "$20.00",
+      price: 20,
       imageUrl: "https://via.placeholder.com/150"
     },
     {
       name: "Product 3",
-      price: "$30.00",
+      price: 30,
       imageUrl: "https://via.placeholder.com/150"
     }
   ])
@@ -26,11 +26,15 @@ function ProductList(){
 
       <article className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 mt-4">
         {productArr.map(product => (
-          <ProductCard 
-            name={product.name}
-            price={product.price}
-            imageUrl={product.imageUrl}
-          />
+          product.price > 100 ? <ProductCard 
+          name={`Premium Product: ${product.name}`}
+          price={product.price}
+          imageUrl={product.imageUrl}
+        /> : <ProductCard 
+        name={product.name}
+        price={product.price}
+        imageUrl={product.imageUrl}
+      />
         ))}
       </article>
     </section>
