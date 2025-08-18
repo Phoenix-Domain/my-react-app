5fd0f43b10960b0d327d66df506e65adb20b2559
Batubo Victory <victorybatubo76@gmail.com>
2025-08-18 15:34:59 +0100

    Finish form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,25 +1,27 @@
-import { useState } from 'react';
-
-function Form(){
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [message, setMessage] = useState('');
+function Form({ setName, setEmail, setMessage, handleSubmit }){
+  
 
   return(
-    <form action="">
+    <form action="" onSubmit={(e) => handleSubmit(e)}>
       <article>
         <label> Name:
-          <input type="text" className='border-1 block mb-4 p-0.5 bg-gray-300' />
+          <input type="text" className='border-1 block mb-4 p-0.5 bg-gray-300' onChange={e => {
+            setName(e.target.value)
+          }}/>
         </label>
         <label>Email
-          <input type="text" className='border-1 block mb-4 p-0.5 bg-gray-300' />
+          <input type="text" className='border-1 block mb-4 p-0.5 bg-gray-300' onChange={e => {
+            setEmail(e.target.value)
+          }}/>
         </label>
         <label>Message
-          <input type="text" className='border-1 block mb-4 p-0.5 bg-gray-300' />
+          <input type="text" className='border-1 block mb-4 p-0.5 bg-gray-300' onChange={e => {
+            setMessage(e.target.value)
+          }}/>
         </label>
       </article>
 
-      <button className="text-white font-bold bg-blue-700 py-2 px-4 rounded-full active:bg-red-700 mb-4">
+      <button className="text-white font-bold bg-blue-700 py-2 px-4 rounded-full active:bg-red-700 mb-4" type='submit'>
         Submit
       </button>
     </form>
