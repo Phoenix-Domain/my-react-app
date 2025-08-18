1c10f334aa60b10109cfb9755537944520cfd388
Batubo Victory <victorybatubo76@gmail.com>
2025-08-18 15:35:23 +0100

    Lift state up

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
 import Form from "./components/Form"
 import Result from "./components/Result"
+import { useState } from 'react';
 
 function App() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+  const [isSubmitted, setIsSubmitted] = useState(false)
+
+  function handleSubmit(e){
+    e.preventDefault();
+    setIsSubmitted(true);
+
+    setName('');
+    setEmail('');
+    setMessage('');
+  }
 
   return (
    <>
-    <Form />
-    <Result />
+    <Form setName={setName} setEmail={setEmail} setMessage={setMessage} handleSubmit={handleSubmit}/>
+
+    <Result name={name} email={email} message={message} isSubmitted={isSubmitted} />
    </>
   )
 }
