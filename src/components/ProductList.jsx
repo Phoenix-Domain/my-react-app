60ad04fc91c69dcf158a9bf6f6952678d4ec2f50
Batubo Victory <victorybatubo76@gmail.com>
2025-08-18 14:37:57 +0100

    Import key prop and change img alt attribute

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -2,14 +2,36 @@ import ProductCard from "./ProductCard";
 import { useState } from "react";
 
 function ProductList(){
-  const [productArr, setProductArr] = useState([])
+  const [productArr, setProductArr] = useState([
+    {
+      name: "Product 1",
+      price: "$10.00",
+      imageUrl: "https://via.placeholder.com/150"
+    },
+    {
+      name: "Product 2",
+      price: "$20.00",
+      imageUrl: "https://via.placeholder.com/150"
+    },
+    {
+      name: "Product 3",
+      price: "$30.00",
+      imageUrl: "https://via.placeholder.com/150"
+    }
+  ])
   
   return(
     <section>
       <h1>Product List App</h1>
 
-      <article>
-        <ProductCard />
+      <article className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 mt-4">
+        {productArr.map((product, index) => (
+          <ProductCard 
+            name={product.name}
+            price={product.price}
+            imageUrl={product.imageUrl}
+          />
+        ))}
       </article>
     </section>
   )
