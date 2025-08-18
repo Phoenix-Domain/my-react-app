236910b74e1488bfab7edf35465db921db1e1d7c
Batubo Victory <victorybatubo76@gmail.com>
2025-08-18 14:54:18 +0100

    Create Form component

---
+++ src/components/Form.jsx
@@ -0,0 +1,23 @@
+function Form(){
+  return(
+    <form action="">
+      <article>
+        <label> Name:
+          <input type="text" />
+        </label>
+        <label>Email
+          <input type="text" />
+        </label>
+        <label>Message
+          <input type="text" />
+        </label>
+      </article>
+
+      <button>
+        Submit
+      </button>
+    </form>
+  )
+}
+
+export default Form
