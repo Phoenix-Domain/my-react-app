6d9ff8692dafa42d1c27f17ecd86522df77ce856
Batubo Victory <victorybatubo76@gmail.com>
2025-08-18 14:56:42 +0100

    Create result component

---
+++ src/components/Result.jsx
@@ -0,0 +1,17 @@
+function Result({ name, email, message}){
+  return(
+    <section>
+      <h1>
+        My name is {name}
+      </h1>
+
+      <p>
+        My email is {email}
+      </p>
+
+      <p>
+        My message is {message}
+      </p>
+    </section>
+  )
+}
