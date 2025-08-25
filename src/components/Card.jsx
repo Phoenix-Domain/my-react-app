598b17e289a316c23bc263ef5b83b14b63e63c9d
Batubo Victory <victorybatubo76@gmail.com>
2025-08-25 13:16:54 +0100

    Edit: Style card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,9 @@
 function Card({ name, bio }){
   return(
-    <div className="">
-      <h1>{name}</h1>
-      <p>{bio}</p>
-      <button>Follow</button>
+    <div className="my-4 w-fit p-2 flex flex-col items-start bg-purple-600 mx-auto rounded-xl">
+      <h1 className='text-white font-bold text-5xl mb-3'>{name}</h1>
+      <p className="text-gray-300 text-xl">{bio}</p>
+      <button className="bg-blue-700 py-2 px-4 mt-4 rounded-xl text-white">Follow</button>
     </div>
   )
 }
