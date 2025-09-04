ccfa3629a0eaddc841445e12b5ac42b3138a185c
Batubo Victory <victorybatubo76@gmail.com>
2025-09-04 11:50:01 +0100

    Edit: Create function to toggle showMore state

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,14 +2,26 @@ import { useState } from "react";
 
 function Card({ name, bio, role, about }){
   const [showMore, setShowMore] = useState(false);
-  
+
+  function toggleShowMore(){
+    setShowMore(!showMore)
+  }
+
   return(
     <div className="my-4 w-fit p-2 flex flex-col items-start bg-purple-600 mx-auto rounded-xl">
       <h1 className='text-white font-bold text-5xl mb-3'>{name}</h1>
 
-      <p className="text-gray-300 text-xl">{bio}</p>
+      <p className="text-gray-300 text-xl my-2">{bio}</p>
 
-      <button className="bg-blue-700 py-2 px-4 mt-4 rounded-xl text-white">Show More &darr;</button>
+      {
+        showMore && <article>
+          <p className="text-gray-300 text-xl my-2">{role}</p>
+
+          <p className="text-gray-300 text-xl my-2">{about}</p>
+        </article>
+      }
+
+      <button className="bg-blue-700 py-2 px-4 mt-4 rounded-xl text-white" onClick={() => toggleShowMore()}>Show More &darr;</button>
     </div>
   )
 }
