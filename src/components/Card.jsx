b4dbf3c7ad976f1d2782f5e78bb7bdf7ebf27514
Batubo Victory <victorybatubo76@gmail.com>
2025-09-04 11:43:07 +0100

    Edit: Create showMore state variable

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 
 function Card({ name, bio, role, about }){
+  const [showMore, setShowMore] = useState(false);
+  
   return(
     <div className="my-4 w-fit p-2 flex flex-col items-start bg-purple-600 mx-auto rounded-xl">
       <h1 className='text-white font-bold text-5xl mb-3'>{name}</h1>
