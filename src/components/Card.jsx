137547d7f2ca751aea48b2a0c710ebfaa5c789f7
Batubo Victory <victorybatubo76@gmail.com>
2025-09-04 11:42:10 +0100

    Edit; Import useState

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,13 @@
-function Card({ name, bio }){
+import { useState } from "react";
+
+function Card({ name, bio, role, about }){
   return(
     <div className="my-4 w-fit p-2 flex flex-col items-start bg-purple-600 mx-auto rounded-xl">
       <h1 className='text-white font-bold text-5xl mb-3'>{name}</h1>
+
       <p className="text-gray-300 text-xl">{bio}</p>
-      <button className="bg-blue-700 py-2 px-4 mt-4 rounded-xl text-white">Show More</button>
+
+      <button className="bg-blue-700 py-2 px-4 mt-4 rounded-xl text-white">Show More &darr;</button>
     </div>
   )
 }
